b8826c68cd1fc50d7bb7b61219060b110f865216
Chad Hurley <cehurley@gmail.com>
2015-09-15 17:19:46 -0400

    ping

---
+++ pimpy/servers/server.py
@@ -0,0 +1,29 @@
+__author__ = 'churley'
+
+
+from tcpserver import *
+from config.settings import settings
+from storage.StorageController import StorageController
+import importlib
+
+class PimpyServer(object):
+
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
+        self.engines = settings.ENGINES
+        self.storage = StorageController(self)
+        lib = importlib.import_module(self.engines['memory']['class'][0])
+        storage_class = getattr(lib,  self.engines['memory']['class'][1])
+        self.storage.ram_store = storage_class()
+        if settings.DEBUG == 1:
+            print self.storage.debugReport()
+
+    def run(self):
+        print 'starting tcp server'
+        server = EchoServer(self)
+        print 'started!!!!'
+        asyncore.loop()
+
+    def cmd(self, msg):
+        print 'in main app: '+cmd
